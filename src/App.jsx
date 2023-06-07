d7aac9c03f547849a4081ed2582c2b3add747671
PhoePA <pyaephyoaung.wcs@gmail.com>
2023-06-07 08:04:15 +0630

    counter project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="logo">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const isShow = useSelector((state) => state.isShow);
+
+  // handling buttons
+  const increaseHandler = () => {
+    dispatch({ type: "increase" });
+  };
+  const decreaseHandler = () => {
+    dispatch({ type: "decrease" });
+  };
+  const increaseBy5Handler = () => {
+    dispatch({ type: "increaseBy5", amount: 5 });
+  };
+  const toggleHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
+  return (
+    <section>
+      <h3>Redux Counter</h3>
+      {isShow && <h1>{counter}</h1>}
+      <hr />
+      <div>
+        <button onClick={increaseHandler}>Increase</button>
+        <button onClick={increaseBy5Handler}>IncreaseBy5</button>
+        <button onClick={decreaseHandler}>Decrease</button>
+        <button onClick={toggleHandler}>Toggle Counter</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
